92e08c12b082556dc052faa31968c8d2abec15e0
Dmitry Sorin <dsorin@yandex-team.ru>
2016-04-12 22:29:14 +0300

    Add UNDO/REDO tasks

--- reducer.js
+++ reducer.js
@@ -45,7 +45,7 @@ const todoApp = (state = initialState, action) => {
                     finished: true,
                     id: newCutHistory[itemIndex].id,
                     title: newCutHistory[itemIndex].title
-                }
+                },
                 ...newCutHistory.slice(itemIndex + 1)
             ];
 
@@ -77,12 +77,31 @@ const todoApp = (state = initialState, action) => {
             break;
 
         case 'REMOVE':
+            history = [
+                ...newCutHistory.slice(0, newCutHistory.length - 1)
+            ];
+
+            return {
+                history,
+                cursor: history.length - 1
+            };
+
             break;
 
         case 'UNDO':
+            return {
+                cursor: state.cursor - 1,
+                history: state.history
+            };
+
             break;
 
         case 'REDO':
+            return {
+                cursor: state.cursor + 1,
+                history: state.history
+            };
+
             break;
 
         default:
