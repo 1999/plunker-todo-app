60bdf5962f6e41ac44891e68e1fa4a2150a34950
Dmitry Sorin <dsorin@yandex-team.ru>
2016-04-12 23:25:30 +0300

    Add redux-logger for debug purposes

--- static/script.jsx
+++ static/script.jsx
@@ -2,13 +2,17 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import createLogger from 'redux-logger';
 
 import todoApp from '../reducer';
 import App from '../components/app.jsx';
 
-const store = createStore(todoApp);
+const store = createStore(
+    todoApp,
+    applyMiddleware(createLogger())
+);
 
 ReactDOM.render(
     <Provider store={store}><App/></Provider>,
